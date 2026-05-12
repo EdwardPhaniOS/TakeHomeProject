483622e4eb9687733c11abf087ac249918f2effb
EdwardPhan <EdwardPhan2015@gmail.com>
2026-05-12 16:59:19 +0700

    Initial Commit

---
+++ TakeHomeProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TakeHomeProject
+//
+//  Created by Vinh Phan on 12/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TakeHomeProject/TakeHomeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakeHomeProjectApp.swift
+//  TakeHomeProject
+//
+//  Created by Vinh Phan on 12/5/26.
+//
+
+import SwiftUI
+
+@main
+struct TakeHomeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
